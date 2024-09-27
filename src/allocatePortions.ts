fdc1253003b2cc74719847f8f5709068f3192ab0
Jesper Damgaard <jesper@vembyte.dk>
2024-09-27 13:31:15 +0200

    initial

---
+++ src/allocatePortions.ts
@@ -0,0 +1,10 @@
+import { getCheapestCombination } from './getCheapestCombination';
+import { Bundle, Order, User } from './types';
+
+export function allocatePortions(users: User[], bundles: Bundle[]): Order[] {
+
+  // TODO: Calculate the resulting orders based on user's portions and maximum price, and available bundles
+  // use "getCheapestCombination()" to calculate the resulting bundles by number of orders
+
+  return [];
+}
